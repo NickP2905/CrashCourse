2be8d035610dd74f30778003ca5a3b0af4eda8e1
NickP2905 <36735340+NickP2905@users.noreply.github.com>
2025-11-07 14:48:27 +0000

    After S3 L2 - Enemies

---
+++ Source/CrashCourse/Private/Characters/CC_EnemyCharacter.cpp
@@ -0,0 +1,38 @@
+﻿// From Druid Mechanics Course
+
+
+#include "Characters/CC_EnemyCharacter.h"
+
+#include "AbilitySystemComponent.h"
+
+
+ACC_EnemyCharacter::ACC_EnemyCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+	
+}
+
+UAbilitySystemComponent* ACC_EnemyCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+
+void ACC_EnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (!IsValid(GetAbilitySystemComponent()))  return;
+
+	GetAbilitySystemComponent()->InitAbilityActorInfo(this, this);
+
+	if (!HasAuthority())  return;
+
+	GiveStartupAbilities();
+	
+}
+
